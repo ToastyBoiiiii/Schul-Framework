5a0be6b6865a4bddfd6906543e25f91387913160
Toast <90329542+ToastyBoiiiii@users.noreply.github.com>
2023-09-28 20:56:28 +0200

    Revert "Issue 6 hannes"

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const video = document.querySelector("video.hero");
-video.style.top = window.scrollY / 2 + "px";
-
-addEventListener("scroll", () => {
-  video.style.top = window.scrollY / 2 + "px";
-});
